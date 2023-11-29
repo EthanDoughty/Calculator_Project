ebbb80da95581a07b3ea388411809747a66150cd
Barrett Brown <barrettbrown2012@gmail.com>
2023-11-29 15:43:32 -0600

    Set up basic functions to work on

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <string>
 
-
+//Handles processing of expression to other functions then returns answer
 double solver(std::string expression){
     double result{};
+    //Loop to get rid of parentheses & pass equation to term solver then set variable to return
     
     return result;
 }
 
+//PEMDAS Handler
+double expressionParser(std::string expression){
+
+}
+
+//Binary operator solver
+double termSolver(double termOne, double termTwo, std::string operator){
+
+}
+
+
+//Gets expression passes to solver prints returned result
 int main(){
     std::cout << "Enter an expression: ";
     std::string expression;
