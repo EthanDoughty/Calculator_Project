540ce48769f34b15eecb8fe1e73ad821558ae2ac
Barrett Brown <barrettbrown2012@gmail.com>
2023-11-29 23:20:30 -0600

    random things

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 //Handles processing of expression to other functions then returns answer
 double solver(std::string expression){
@@ -17,18 +18,18 @@ bool errorHandler(std::string expression){
     //Check for unmatched & mismatched parentheses/operands
     //Missing operator 5(2+3) missing the *
     //Invalid Characters
-
+    return true;
 }
 
 //PEMDAS Handler Passes to termSolver
 //Parses 5-2*(6-3) and passes 6-3 to term solver resulting in 5-2*3 
     //then 2*3 then passes 5-6, Returns final answer of -1 to solver
-double expressionParser(std::string expression){
+double expressionParser(std::string& expression){
 
 }
 
 //Binary operator solver ex: input 5*5 returns 25
-double termSolver(std::string operation){
+double termSolver(std::string& operation){
     //If division check for divide by 0 error first
 }
 
