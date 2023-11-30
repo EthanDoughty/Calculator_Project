468cc2ef10096ddb9663666401fb1721876c3074
Barrett Brown <barrettbrown2012@gmail.com>
2023-11-29 22:53:12 -0600

    Set up basic code for group.

--- Program/main.cpp
+++ Program/main.cpp
@@ -4,19 +4,32 @@
 //Handles processing of expression to other functions then returns answer
 double solver(std::string expression){
     double result{};
-    //Loop to get rid of parentheses & pass equation to term solver then set variable to return
+    //Pass to Error handler function then to expression parser to solve
+    if (errorHandler(expression)){
+
+    }
     
     return result;
 }
 
-//PEMDAS Handler
+//Checks for basic errors returns true if error free
+bool errorHandler(std::string expression){
+    //Check for unmatched & mismatched parentheses/operands
+    //Missing operator 5(2+3) missing the *
+    //Invalid Characters
+
+}
+
+//PEMDAS Handler Passes to termSolver
+//Parses 5-2*(6-3) and passes 6-3 to term solver resulting in 5-2*3 
+    //then 2*3 then passes 5-6, Returns final answer of -1 to solver
 double expressionParser(std::string expression){
 
 }
 
-//Binary operator solver
+//Binary operator solver ex: input 5*5 returns 25
 double termSolver(std::string operation){
-
+    //If division check for divide by 0 error first
 }
 
 
